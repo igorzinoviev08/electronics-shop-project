7e4759b7cee21b23c2694ffb46b9958269d93e93
igorzinoviev08 <zinovievigor08@gmail.com>
2023-07-20 02:06:58 +0300

    HW_4 is exec

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from src.item import Item
-
+from src.phone import Phone
+@pytest.fixture
+def phone():
+    return Phone("iPhone 14", 120_000, 5, 2)
 
 @pytest.fixture
 def item_1():
